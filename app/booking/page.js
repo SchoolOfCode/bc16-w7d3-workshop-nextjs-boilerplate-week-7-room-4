2421363a3bd1d18a50d8a43cf862c8f8535d0c43
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-03-26 11:47:24 +0000

    booking page styled

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,12 +1,14 @@
+import "./page.css";
+
 export default function Page() {
   return (
-    <>
+    <div className="booking-container">
       <div className="booking-title">
         <h2>Design Booking</h2>
       </div>
       <form>
         <p>Personal Information</p>
-        <div>
+        <div className="form-container">
           <label htmlFor="fullName">Full Name</label>
           <input id="fullName" type="text"></input>
           <label htmlFor="postcode">Postcode</label>
@@ -17,7 +19,7 @@ export default function Page() {
           <input id="city" type="text"></input>
         </div>
         <p>Contact Information</p>
-        <div>
+        <div className="form-container">
           <label htmlFor="phoneNumber">Phone Number</label>
           <input id="phoneNumber" type="text"></input>
           <label htmlFor="email">Email Address</label>
@@ -25,6 +27,6 @@ export default function Page() {
         </div>
         <button type="submit">Request Design Consultation</button>
       </form>
-    </>
+    </div>
   );
 }
