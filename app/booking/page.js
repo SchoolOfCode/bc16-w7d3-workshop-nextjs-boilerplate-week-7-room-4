531fb875414f336dff815f251d96144a11d98807
mrw46 <mrw46@outlook.com>
2024-03-26 11:18:48 +0000

    form architecture added

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,7 +1,30 @@
 export default function Page() {
   return (
     <>
-      <p>Booking page here</p>
+      <div className="booking-title">
+        <h2>Design Booking</h2>
+      </div>
+      <form>
+        <p>Personal Information</p>
+        <div>
+          <label htmlFor="fullName">Full Name</label>
+          <input id="fullName" type="text"></input>
+          <label htmlFor="postcode">Postcode</label>
+          <input id="postcode" type="text"></input>
+          <label htmlFor="house">House/Flat Number and Street Name</label>
+          <input id="house" type="text"></input>
+          <label htmlFor="city">City</label>
+          <input id="city" type="text"></input>
+        </div>
+        <p>Contact Information</p>
+        <div>
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input id="phoneNumber" type="text"></input>
+          <label htmlFor="email">Email Address</label>
+          <input id="email" type="text"></input>
+        </div>
+        <button type="submit">Request Design Consultation</button>
+      </form>
     </>
   );
 }
