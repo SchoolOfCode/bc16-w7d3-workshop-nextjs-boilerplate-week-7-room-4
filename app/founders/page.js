e30413f454c5163b3940ca52829b7a40025c886f
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-03-27 09:43:27 +0000

    updated styles on founders

--- app/founders/page.js
+++ app/founders/page.js
@@ -2,39 +2,39 @@ import ReverseArticle from "@/src/reverse-article/reverse-article";
 import "./page.css";
 
 export default function Page() {
-  return (
-    <div className="founders-container">
-      <div className="founders-orange-box">
-        <h2>Meet the artisans behind our masterpieces!</h2>
-        <h2>Mike and Mandy</h2>
-        <img src="/founder-mike-and-mandy.png"></img>
-      </div>
-      <div className="founders-card">
-        <h2>Our craftsmanship</h2>
-        <p>
-          Mike and Mandy studied and honed their craft under the fireplace
-          sensei Vik Von Blaze. Nothing gets delivered to a customer without
-          their sign off.
-        </p>
-        <img src="/founders-1.png"></img>
-      </div>
-      <div className="founders-card">
-        <h2>Our experience</h2>
-        <p>
-          Numbers don't lie - we've been around for 20+ years and have a long
-          list of happy customers who gladly recommend us.
-        </p>
-        <img src="/founders-2.png"></img>
-      </div>
-      <div className="founders-card">
-        <h2>Our guarantee</h2>
-        <p>
-          If you're not 100% satisfied we will fully refund your purchase. Also,
-          we offer free repairs for the first 20 years of ownership. Find that
-          somewhere else!
-        </p>
-        <img src="/founders-3.png"></img>
-      </div>
-    </div>
-  );
+	return (
+		<div className="founders-container">
+			<div className="founders-orange-box">
+				<h2 id="foundersH2">Meet the artisans behind our masterpieces!</h2>
+				<h2 id="foundersH2">Mike and Mandy</h2>
+				<img src="/founder-mike-and-mandy.png"></img>
+			</div>
+			<div className="founders-card">
+				<h2 id="foundersH2">Our craftsmanship</h2>
+				<p>
+					Mike and Mandy studied and honed their craft under the fireplace
+					sensei Vik Von Blaze. Nothing gets delivered to a customer without
+					their sign off.
+				</p>
+				<img src="/founders-1.png"></img>
+			</div>
+			<div className="founders-card">
+				<h2 id="foundersH2">Our experience</h2>
+				<p>
+					Numbers don't lie - we've been around for 20+ years and have a long
+					list of happy customers who gladly recommend us.
+				</p>
+				<img src="/founders-2.png"></img>
+			</div>
+			<div className="founders-card">
+				<h2 id="foundersH2">Our guarantee</h2>
+				<p>
+					If you're not 100% satisfied we will fully refund your purchase. Also,
+					we offer free repairs for the first 20 years of ownership. Find that
+					somewhere else!
+				</p>
+				<img src="/founders-3.png"></img>
+			</div>
+		</div>
+	);
 }
--- src/hero-section/hero-section.jsx
+++ src/hero-section/hero-section.jsx
@@ -1,29 +1,28 @@
-import "./hero-section.css"
-import 'react'
+import "./hero-section.css";
+import "react";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function HeroSection(){
-    return (
-        <section className="hero-section">
-            <Image
-                className="hero-image"
-                width={10000}
-                height={10000}
-                src="/hero-mobile.png"
-                alt="Fireplace Image"
-            />
+export default function HeroSection() {
+	return (
+		<section className="hero-section">
+			<Image
+				className="hero-image"
+				width={10000}
+				height={10000}
+				src="/hero-mobile.png"
+				alt="Fireplace Image"
+			/>
 
-            <div className="orange-box-container">
-                <section className="orange-box">
-                    <h1 id="orange-box-heading">Discover the Perfect Fireplace ...</h1>
-                    <p id="contact-info">
-                        Book consultation:{" "}
-                        <span><Link href='/booking'>Bookings Page</Link></span>
-                    </p>
-                </section>
-            </div>
-        </section>
-    )
+			<div className="orange-box-container">
+				<section className="orange-box">
+					<h1 id="orange-box-heading">Discover the Perfect Fireplace ...</h1>
+					<p id="contact-info">
+						<Link href="/booking">Book a consultation today!</Link>
+						<span></span>
+					</p>
+				</section>
+			</div>
+		</section>
+	);
 }
-
